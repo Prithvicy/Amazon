1765878c35fb56b1870f70bc42c905d08cc49b84
Prithvicy <prithvi40201@gmail.com>
2021-10-26 23:47:17 +0530

    trail change

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <nav className="header">
-            <img className="header__image" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            <img className="header__image" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="123" />
 
         </nav>
    
