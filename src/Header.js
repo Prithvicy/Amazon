30874cc13cc78f21f773fe5f8ba016b3078ee662
Prithvicy <prithvi40201@gmail.com>
2021-10-26 23:25:38 +0530

    app.js commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <nav className="header">
+            <img className="header__image" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+
+        </nav>
+   
+    )
+}
+
+export default Header
